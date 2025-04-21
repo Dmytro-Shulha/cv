bdabf27aad1e1eb0e5bcbfc18d921a42eefb0684
Dmytro Shulha <dimashulga1115@gmail.com>
2025-04-22 00:13:58 +0300

    Made calculated durations for positions and for employer.
    Using plain JS, Document API and Luxon.
    No node.js required :)
    Hugo templates, unfortunately, weren't enough for date subtraction.

---
+++ assets/js/_experience.js
@@ -0,0 +1,64 @@
+document.addEventListener("DOMContentLoaded", () => {
+    var DateTime = luxon.DateTime;
+    var Duration = luxon.Duration;
+
+    document.querySelectorAll(".experience__item").forEach((experienceWrapper) => {
+        const experienceDateWrapper = experienceWrapper.querySelectorAll(".experience__date");
+        console.log(`experienceWrapper:`, experienceWrapper);
+        const positions = Array.from(experienceDateWrapper).map((position) => {
+
+            const start = position.querySelector(".experience__date__start")?.textContent?.trim();
+            const end = position.querySelector(".experience__date__end")?.textContent?.trim();
+
+            const startDate = DateTime.fromFormat(start, "MMM yyyy", { locale: "en" });
+            const currentDate = DateTime.now().toFormat("MMM yyyy")
+            const endDate = DateTime.fromFormat((!end || end === 'Current') ? currentDate : end, "MMM yyyy", { locale: "en" }).plus({ months: 1 });
+
+            let durationDiff = endDate?.diff(startDate, ['years', 'months']);
+            console.log(durationDiff);
+            durationDiff = durationDiff.toFormat(`${durationDiff.years >= 1 ? "y'y' " : ""}${durationDiff.months >= 1 ? "M'm'" : ""}`);
+            if (experienceDateWrapper.length > 1) {
+                position.querySelector(".experience__date__duration").textContent = `${durationDiff} ~ `;
+            }
+
+            console.log(`start: ${start}`);
+            console.log(`end: ${end}`);
+            console.log(`durationDiff: ${durationDiff}`);
+
+            return {
+                start: startDate,
+                end: endDate,
+                duration: durationDiff,
+            }
+        });
+        console.log(`positions:`, positions);
+        const positionStart = DateTime.min(...positions.map((pos) => pos.start));
+        const positionEnd = DateTime.max(...positions.map((pos) => pos.end));
+        const positionDuration = positionEnd?.diff(positionStart, ['years', 'months']);
+        console.log(`positionStart: ${positionStart}`);
+        console.log(`positionEnd: ${positionEnd}`);
+        const positionDurationSlot = experienceWrapper.querySelector(".experience__duration");
+        if (positionDurationSlot) {
+            positionDurationSlot.textContent = positionDuration?.toFormat(`
+                ${positionDuration.years >= 1 ? "y'y' " : ""}
+                ${positionDuration.months >= 1 ? "M'm'" : ""}
+            `) || "";
+        }
+
+        // for (const wrapper of document.querySelectorAll(".experience__date")) {
+        //     const start = wrapper.querySelector(".experience__date__start")?.textContent?.trim();
+        //     const end = wrapper.querySelector(".experience__date__end")?.textContent?.trim();
+
+        //     const startDate = DateTime.fromFormat(start, "MMM yyyy", { locale: "en" });
+        //     const currentDate = DateTime.now().toFormat("MMM yyyy")
+        //     const endDate = DateTime.fromFormat((!end || end === 'Current') ? currentDate : end, "MMM yyyy", { locale: "en" }).plus({ months: 1 });
+
+        //     let durationDiff = endDate.diff(startDate, ['years', 'months']);
+        //     console.log(durationDiff);
+        //     durationDiff = durationDiff.toFormat(`${durationDiff.years >= 1 ? "y'y' " : ""}${durationDiff.months >= 1 ? "M'm'" : ""}`);
+        //     wrapper.querySelector(".experience__date__duration").textContent = durationDiff;
+        // }
+
+
+    });
+});
